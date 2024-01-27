1bb2e1ea6010e9a9ebcb093089dc46fee52211a2
Taras Tereschenko <tarrrasio@gmail.com>
2024-01-27 16:34:48 +0500

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { stat, writeFile } from 'node:fs/promises';
+
 const create = async () => {
-    // Write your code here 
+	const filePath = './src/fs/files/fresh.txt';
+	const fileContent = 'I am fresh and young';
+
+	try {
+		await stat(filePath);
+		throw new Error('FS operation failed');
+	} catch (error) {
+		if (error.code === 'ENOENT') {
+			await writeFile(filePath, fileContent);
+		} else {
+			throw error;
+		}
+	}
 };
 
 await create();
