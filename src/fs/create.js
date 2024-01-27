ab31a1bf3357bc25141bb24546cdd5b1b671e450
Taras Tereschenko <tarrrasio@gmail.com>
2024-01-27 18:06:52 +0500

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,13 +1,14 @@
 import { stat, writeFile } from 'node:fs/promises';
 
 import { isExists } from '../utils/is-exists.js';
+import { FSError } from '../utils/fs-errors.js';
 
 const create = async () => {
 	const filePath = './src/fs/files/fresh.txt';
 	const fileContent = 'I am fresh and young';
 
 	if (await isExists(filePath)) {
-		throw new Error('FS operation failed');
+		throw new FSError();
 	}
 
 	await writeFile(filePath, fileContent);
