90af5b9e7eb0de5bc1d9c9c76a55f0ee944603d0
Taras Tereschenko <tarrrasio@gmail.com>
2024-01-28 14:56:36 +0500

    feat: implement parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+	const rssArgs = [];
+	for (const key in process.env) {
+		if (key.startsWith('RSS_')) {
+			rssArgs.push(`${key}=${process.env[key]}`);
+		}
+	}
+	console.log(rssArgs.join('; '));
 };
 
 parseEnv();
