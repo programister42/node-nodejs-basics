58deadb0acb682fb2fb7200b6dcd001311cc124c
Taras Tereschenko <tarrrasio@gmail.com>
2024-01-28 12:56:23 +0500

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import { rename as fsRename } from 'node:fs/promises';
+
+import { FSError } from '../utils/fs-errors.js';
+import { isExists } from '../utils/is-exists.js';
+
 const rename = async () => {
-    // Write your code here 
+	const fileURL = new URL('./files/wrongFilename.txt', import.meta.url);
+	const newFileURL = new URL('./files/properFilename.md', import.meta.url);
+
+	if (!(await isExists(fileURL)) || (await isExists(newFileURL))) {
+		throw new FSError();
+	}
+
+	try {
+		await fsRename(fileURL, newFileURL);
+	} catch (error) {
+		throw new FSError();
+	}
 };
 
 await rename();
