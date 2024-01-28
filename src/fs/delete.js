c04c94a156b7b32924ec3b86eccd7e99189ed238
Taras Tereschenko <tarrrasio@gmail.com>
2024-01-28 13:04:16 +0500

    feat: implement remove function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import { rm } from 'node:fs/promises';
+
+import { FSError } from '../utils/fs-errors.js';
+
 const remove = async () => {
-    // Write your code here 
+	const fileURL = new URL('./files/fileToRemove.txt', import.meta.url);
+
+	try {
+		await rm(fileURL);
+	} catch (error) {
+		throw new FSError();
+	}
 };
 
 await remove();
