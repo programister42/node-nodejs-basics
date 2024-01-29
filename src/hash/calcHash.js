d6ff2b41d979174b194ac02faf027279c8c657ce
Taras Tereschenko <tarrrasio@gmail.com>
2024-01-29 23:55:47 +0500

    feat: implement calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { URL } from 'node:url';
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+	const fileURL = new URL(
+		'./files/fileToCalculateHashFor.txt',
+		import.meta.url
+	);
+
+	const hash = createHash('sha256');
+	const input = createReadStream(fileURL);
+
+	await pipeline(input, hash);
+
+	console.log(hash.digest('hex'));
 };
 
 await calculateHash();
