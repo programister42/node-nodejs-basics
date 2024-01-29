07307dddd999734385489838b1b962aff768ef1e
Taras Tereschenko <tarrrasio@gmail.com>
2024-01-30 00:00:35 +0500

    feat: implement read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+
 const read = async () => {
-    // Write your code here 
+	const fileURL = new URL('./files/fileToRead.txt', import.meta.url);
+	const readStream = createReadStream(fileURL);
+
+	readStream.on('end', () => {
+		process.stdout.write('\n');
+	});
+
+	await pipeline(readStream, process.stdout);
 };
 
 await read();
