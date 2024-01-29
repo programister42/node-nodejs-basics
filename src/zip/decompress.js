8fe5277f1761e50fef8564225bb5d4de4626cdcc
Taras Tereschenko <tarrrasio@gmail.com>
2024-01-30 00:29:48 +0500

    feat: implement decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import { URL } from 'node:url';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+
 const decompress = async () => {
-    // Write your code here 
+	const sourceURL = new URL('./files/archive.gz', import.meta.url);
+	const destinationURL = new URL(
+		'./files/fileToCompress.txt',
+		import.meta.url
+	);
+	const readStream = createReadStream(sourceURL);
+	const writeStream = createWriteStream(destinationURL);
+	const gunzip = createGunzip();
+
+	await pipeline(readStream, gunzip, writeStream);
 };
 
 await decompress();
